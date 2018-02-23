5e59c539a52d854cdce098b4c4715ad4d4e246d5
James <james@team-wombat.com>
2018-02-22 20:45:00 -0700

    User now has name, phone, can add other attributes. Can update user with

--- js/profile/profile.controller.js
+++ js/profile/profile.controller.js
@@ -6,14 +6,23 @@
         .module("petabl")
         .controller("ProfileController", [
             "$stateParams",
+            "$scope",
+            "$auth",
             ProfileControllerFunction
         ])
 
-    function ProfileControllerFunction($stateParams) {
+    function ProfileControllerFunction($stateParams, $scope, $auth) {
         console.log("I'm in the profile controller!")
         var vm = this;
-
-
+        console.log($scope.user.id);
+        // vm.handleRegBtnClick = function() {
+        //     $auth.updateAccount($scope.profileForm).then(function(resp) {
+        //         console.log("update successful");
+        //     })
+        //     .catch(function(resp) {
+        //         console.log("update failed", resp);
+        //     });
+        // };
     }
 
 // END OF IIFE
--- js/user_registrations/userregistrations.controller.js
+++ js/user_registrations/userregistrations.controller.js
@@ -22,9 +22,24 @@
             $auth.submitRegistration($scope.registrationForm)
                 .then(function(resp) {
                     console.log("auth success");
-                    vm.user = resp;
+
+                    $scope.updateAccountForm = {
+                        "firstname": $scope.registrationForm.firstname,
+                        "lastname": $scope.registrationForm.lastname,
+                        "phone1": $scope.registrationForm.phone1
+                    }
+
                     // alert('Welcome ', vm.user.email, '!');
                 }).then(function() {
+                    console.log($scope.updateAccountForm);
+                    $auth.updateAccount($scope.updateAccountForm).then(function(resp) {
+                        console.log("update successful");
+                    })
+                    .catch(function(resp) {
+                        console.log("update failed", resp);
+                    });
+                })
+                .then(function() {
                 $auth.submitLogin({
                     email: $scope.registrationForm.email,
                     password: $scope.registrationForm.password
